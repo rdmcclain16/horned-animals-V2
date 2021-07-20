b43f71b69ae8e1b798321ec20d05b772b80c6773
SbR-one <68614925+SbR-one@users.noreply.github.com>
2021-07-19 23:20:25 -0400

    pulling data from data.js

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function HornedBeast(props) {
+function HornedBeast({image_url, title, description}) {
+  console.log(title);
   return (
     <>
-      <h2>{props.title}</h2>
-      <img height="500px" alt="bird picture" title="Picture of a bird" src={props.imageURL}/>;
-      <p>{props.desc}</p>
+      <h2>{title}</h2>
+      <img height="500px" alt={title} title="Picture of a bird" src={image_url}/>
+      <p>{description}</p>
     </>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import Parrot from '../images/parrot-2756488_1920.jpg';
+// import Parrot from '../images/parrot-2756488_1920.jpg';
+import { animalData } from '../data';
 
 function Main() {
+  // console.log(animalData);
   return (
     <>
-      <HornedBeast title="Parrot" imageURL={Parrot} desc="Colorful, loud, trpoical bird."/>
-      <HornedBeast/>
+      {/* <HornedBeast title="Parrot" imageURL={Parrot} desc="Colorful, loud, trpoical bird."/> */}
+      {/* <HornedBeast/> */}
+
+      {animalData.map((data, key) => {
+        // console.log(data.title);
+        return (
+          <div key={key}>
+            <HornedBeast
+              key={key}
+              image_url={data.image_url}
+              title={data.title}
+              description={data.description}
+            />
+          </div>
+        );
+      })}
     </>
   );
 }
