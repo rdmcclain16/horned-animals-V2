4cacb7067e2697effdba26bd06ec109080707f4b
SbR-one <68614925+SbR-one@users.noreply.github.com>
2021-07-20 21:00:00 -0400

    Feature 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-// import Parrot from '../images/parrot-2756488_1920.jpg';
 import { animalData } from '../data';
 
 function Main() {
   // console.log(animalData);
   return (
     <>
-      {/* <HornedBeast title="Parrot" imageURL={Parrot} desc="Colorful, loud, trpoical bird."/> */}
-      {/* <HornedBeast/> */}
-
       {animalData.map((data, key) => {
-        // console.log(data.title);
         return (
           <div key={key}>
             <HornedBeast
