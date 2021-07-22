46af9cabbfaf2479ba0e784eac785fe583e81527
SbR-one <68614925+SbR-one@users.noreply.github.com>
2021-07-21 21:21:52 -0400

    Feature 2

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BsFillHeartFill} from 'react-icons/bs';
+
 const { useState } = React;
 
 function HornedBeast({image_url, title, description}) {
@@ -11,9 +13,23 @@ function HornedBeast({image_url, title, description}) {
   return (
     <>
       <h2>{title}</h2>
-      <img height="500px" alt={title} title="Picture of a bird" src={image_url}/>
+
       <p>{description}</p>
-      <button onClick={handleClick}>Favorite {counter}</button>
+
+      <button onClick={handleClick}>
+        <img 
+          height="500px" 
+          alt={title} 
+          onClick={handleClick}
+          title="Picture of a bird" 
+          src={image_url}>
+        </img>
+        <BsFillHeartFill />
+        {counter}
+        {/* <div style={{backgroundImage: `url(${image_url})`, objectFit: "cover", height: "500px", width: "500px"}}>
+          {counter}
+        </div> */}
+      </button>
     </>
   );
 }
