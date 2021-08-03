050502a88f4b930c2d803860f542dd1ac308d9cd
rdmcclain16 <74519618+rdmcclain16@users.noreply.github.com>
2021-08-02 21:22:24 -0500

    added dropdown menu for lab 4

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Dropdown} from 'react-bootstrap';
 import HornedBeast from './HornedBeast';
-// import SelectedBeast from './SelectedBeast';
 
 function Main({animalData}) {
   return (
     <>
       <Container>
         <Row>
+          <Col xs={12}>
+            <Form>
+              <Form.Group>
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" placeholder="Please Enter Your Name" />
+                <Form.Text>Raven McClain </Form.Text>
+              </Form.Group>
+            </Form>
+          </Col>
+        
+          <Col xs={12}>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="Filter Dropdown">
+                Filter Beast by Horn Amount
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item>1 Horn</Dropdown.Item>
+                <Dropdown.Item>2 Horns</Dropdown.Item>
+                <Dropdown.Item>3 Horns</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+
           {animalData.map((data, key) => {
             return (
               <Col xs={12} md={6} lg={4}>
